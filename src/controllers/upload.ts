3d461357cbe8d657e6628acb850452b899afe0b4
kimn <a9523023650@gmail.com>
2023-10-14 17:23:10 +0800

    [修改] getImageUrl 改假資料

--- src/controllers/upload.ts
+++ src/controllers/upload.ts
@@ -7,18 +7,13 @@ export const getImageUrl: RequestHandler = async (req, res) => {
 
     const formData = new FormData();
     formData.append('image', new Blob([Buffer.from(req.file.buffer)]));
-    formData.append('album', process.env.IMGUR_ALBUM_ID);
 
-    const { data } = await fetch('https://api.imgur.com/3/image', {
-        method: 'POST',
-        body: formData,
-        headers: {
-            Authorization: `Bearer ${process.env.IMGUR_REFRESH_TOKEN}`
-        }
-    }).then(value => value.json());
+    await new Promise(resolve => setTimeout(resolve, 1000));
 
     res.send({
         status: true,
-        result: data
+        result: {
+            link: 'https://fakeimg.pl/300/'
+        }
     });
 };

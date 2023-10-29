92d84c34284ce8b428be51d947913af51cd58065
kimn <a9523023650@gmail.com>
2023-10-30 00:39:48 +0800

    [修改] 簡化 dotenv

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import '@/app/env';
+import 'dotenv/config';
 import '@/app/connection';
 import Routes from '@/routes';
 import * as Exception from '@/app/exception';

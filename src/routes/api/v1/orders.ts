9f426e68bc5d19f738b13f2ee2ce31f2e2c32067
ptp_ting-xuan <ptp_ting-xuan@photons.com.tw>
2023-10-31 12:47:11 +0800

    [修改] 新增訂單時 populate roomId

--- src/routes/api/v1/orders.ts
+++ src/routes/api/v1/orders.ts
@@ -140,7 +140,24 @@ router.post(
             schema: {
                 "status": true,
                 "result": {
-                    "roomId": "65251f6095429cd58654bf12",
+                    "roomId": {
+                        "_id": "65251f6095429cd58654bf12",
+                        "name": "尊爵雙人房",
+                        "description": "享受高級的住宿體驗，尊爵雙人房提供給您舒適寬敞的空間和精緻的裝潢。",
+                        "content": "私人衛浴 • 市景 • 免費盥洗用品 • 淋浴間 • 空調 • 毛巾 • 床單 • 床邊插座....",
+                        "imageUrl": "https://fakeimg.pl/300/",
+                        "imageUrlList": [
+                            "https://fakeimg.pl/300/",
+                            "https://fakeimg.pl/300/",
+                            "https://fakeimg.pl/300/"
+                        ],
+                        "areaInfo": "24坪",
+                        "bedInfo": "一張大床",
+                        "maxPeople": 4,
+                        "price": 10000,
+                        "createdAt": "2023-10-10T09:54:40.063Z",
+                        "updatedAt": "2023-10-10T09:54:40.063Z"
+                    },
                     "checkInDate": "2023-06-17T16:00:00.000Z",
                     "checkOutDate": "2023-06-18T16:00:00.000Z",
                     "peopleNum": 2,
